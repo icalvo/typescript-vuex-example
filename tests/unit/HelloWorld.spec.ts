ddbe631c96cc2ceabd6c54f8806b5461d3425b26
Ignacio Calvo <ignacio.calvo@frontiersin.org>
2019-05-20 11:53:32 +0200

    Have a working component test

--- tests/unit/HelloWorld.spec.ts
+++ tests/unit/HelloWorld.spec.ts
@@ -1,12 +1,22 @@
-import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/Profile.vue';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import Profile from '@/components/Profile.vue';
+import store from '@/store/store';
+import Vuetify from 'vuetify';
+import { AxiosPlugin } from '@/axios-plugin';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+localVue.use(Vuetify);
+localVue.use(AxiosPlugin);
+
+describe('Profile.vue', () => {
+  it('can be instanced', () => {
+    const wrapper = shallowMount(Profile, {
+      store,
+      localVue,
     });
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.isVueInstance()).toBeTruthy();
   });
 });
